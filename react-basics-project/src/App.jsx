42e2853dab1d010769e1f482423e841a69eef334
OOW81 <118228607+OOW81@users.noreply.github.com>
2024-02-28 01:35:36 +0100

    Update files: search and click

--- react-basics-project/src/App.jsx
+++ react-basics-project/src/App.jsx
@@ -1,6 +1,29 @@
 import { RecipeListPage } from './pages/RecipeListPage';
+import { RecipePage } from "./pages/RecipePage";
+import { useState } from "react";
+import { Flex } from "@chakra-ui/react";
+import { data } from "./utils/data";
 
 export const App = () => {
   // Your state code here
-  return <RecipeListPage />;
+   const [selectedRecipe, setSelectedRecipe] = useState();
+  
+  return <RecipeListPage />; //(kolla upp vilken variant som e rätt)
+    <>
+      {selectedRecipe ? (
+        <Flex
+          backgroundColor="blue.300"
+          justifyContent="center"
+          alignItems="center"
+        >
+          <RecipePage
+            recipeObject={selectedRecipe}
+            clickFn={setSelectedRecipe}
+          />
+        </Flex>
+      ) : (
+        <RecipeListPage clickFn={setSelectedRecipe} />
+      )}
+    </>
+  );
 };
